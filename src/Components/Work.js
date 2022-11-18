72d48175f3d16bc245b422b5f0542c6ddb551629
JapSir <japsircode@gmail.com>
2022-11-18 12:35:10 +0100

    add basic structure to components

---
+++ src/Components/Work.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class Work extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+
+export default Work
