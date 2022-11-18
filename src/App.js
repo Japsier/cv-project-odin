f4f1775e59d4bba522d23cf86e5a9295e8b111a9
JapSir <japsircode@gmail.com>
2022-11-18 12:29:03 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+import Education from "./Components/Education"
+import Work from "./Components/Work"
+import General from "./Components/General"
+
+class App extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+
+}
+
+export default App;
